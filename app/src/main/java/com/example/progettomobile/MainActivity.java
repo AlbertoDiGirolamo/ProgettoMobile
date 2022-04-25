63eb84496fc80ae25ae7362bf71140c8be6b2c6e
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2022-04-25 16:10:35 +0200

    add add_product

--- app/src/main/java/com/example/progettomobile/MainActivity.java
+++ app/src/main/java/com/example/progettomobile/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.progettomobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.progettomobile.RecyclerView.CardAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_main);
+
+       // setRecyclerView(this);
+        Utilities.insertFragment(this,  new HomeFragment(), HomeFragment.class.getSimpleName());
+
     }
+
+
 }
